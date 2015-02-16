720f7e76820b7337ca04e7ca80e5c25b7da24536
big_Xplosion <bigxplosion44@gmail.com>
2015-02-17 00:19:55 +0100

    Fix wrong registration

--- src/main/java/quadrum/proxy/ClientProxy.java
+++ src/main/java/quadrum/proxy/ClientProxy.java
@@ -4,13 +4,13 @@ import net.minecraft.client.Minecraft;
 
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-import quadrum.Quadrum;
+import quadrum.client.ResourcePackQuadrum;
 
 public class ClientProxy extends CommonProxy {
 
 	@Override
 	public void preInit(FMLPreInitializationEvent event) {
-		Minecraft.getMinecraft().defaultResourcePacks.add(Quadrum.instance);
+		Minecraft.getMinecraft().defaultResourcePacks.add(new ResourcePackQuadrum());
 		super.preInit(event);
 	}
 }
