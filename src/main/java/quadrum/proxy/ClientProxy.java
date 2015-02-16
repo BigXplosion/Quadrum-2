fa50fcf9713f7c9dfdf90b117ca3a7b03cefa12c
big_Xplosion <bigxplosion44@gmail.com>
2015-02-17 00:07:49 +0100

    Added access transformer to allow the registration of the resource pack

--- src/main/java/quadrum/proxy/ClientProxy.java
+++ src/main/java/quadrum/proxy/ClientProxy.java
@@ -1,11 +1,16 @@
 package quadrum.proxy;
 
+import net.minecraft.client.Minecraft;
+
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+import quadrum.Quadrum;
 
 public class ClientProxy extends CommonProxy {
 
 	@Override
 	public void preInit(FMLPreInitializationEvent event) {
+		Minecraft.getMinecraft().defaultResourcePacks.add(Quadrum.instance);
 		super.preInit(event);
 	}
 }
